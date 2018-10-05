7b818c75d17dcf1736830d66d916db5b13b9be0e
Lucas Lombardo <Lucasl@bu.edu>
2018-10-05 18:56:33 -0400

    lints clean

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: "airbnb",
+  parser: "babel-eslint",
+  rules: {
+    "react/jsx-filename-extension": 0,
+    "react/prefer-stateless-function": 0,
+    "react/destructuring-assignment": 0,
+    "function-paren-newline": 0,
+    "linebreak-style": 0,
+    "import/prefer-default-export": 0,
+    "react/jsx-one-expression-per-line": 0
+  }
+};
