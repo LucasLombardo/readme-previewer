2dff255ce50c16d5414a5197ec89319ae5594e60
Lucas Lombardo <Lucasl@bu.edu>
2018-10-07 01:36:23 -0400

    updates editor

--- src/Editor.js
+++ src/Editor.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import './Editor.scss';
 
 export default class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: '# This is a header\n\nAnd this is a paragraph' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+    this.props.updateMarkdown(this.state.input);
+  }
+
   render() {
     return (
       <div className="Editor">
@@ -9,7 +21,11 @@ export default class Editor extends Component {
           <div className="controls">
             <p>Hi</p>
           </div>
-          <textarea className="markdown-input" />
+          <textarea
+            className="markdown-input"
+            value={this.state.input}
+            onChange={this.handleChange}
+          />
         </div>
       </div>
     );
