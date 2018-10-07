f22576de61df81f59ab11fa1e1f22dda9cd7eba6
Lucas Lombardo <Lucasl@bu.edu>
2018-10-07 15:03:54 -0400

    adds code button

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,8 @@ module.exports = {
     "linebreak-style": 0,
     "import/prefer-default-export": 0,
     "react/jsx-one-expression-per-line": 0
+  },
+  env: {
+    browser: true
   }
 };
--- src/Editor.js
+++ src/Editor.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Editor.scss';
 
 export default class Editor extends Component {
@@ -7,11 +8,27 @@ export default class Editor extends Component {
     this.state = { input: '# This is a header\n\nAnd this is a paragraph' };
 
     this.handleChange = this.handleChange.bind(this);
+    this.formatCode = this.formatCode.bind(this);
+
+    this.myRef = React.createRef();
   }
 
   handleChange(event) {
     this.setState({ input: event.target.value });
-    this.props.updateMarkdown(this.state.input);
+    this.props.updateMarkdown(event.target.value);
+  }
+
+  formatCode() {
+    const { input } = this.state;
+    const markdownInput = this.myRef.current;
+    const selection = window.getSelection();
+    const start = markdownInput.selectionStart;
+    const end = markdownInput.selectionEnd;
+    const newInput = `${input.slice(0, start)}\`${selection}\`${input.slice(
+      end,
+    )}`;
+    this.setState({ input: newInput });
+    this.props.updateMarkdown(newInput);
   }
 
   render() {
@@ -19,10 +36,13 @@ export default class Editor extends Component {
       <div className="Editor">
         <div className="edit-box">
           <div className="controls">
-            <p>Hi</p>
+            <button type="button" onClick={this.formatCode}>
+              &lt;/&gt;
+            </button>
           </div>
           <textarea
             className="markdown-input"
+            ref={this.myRef}
             value={this.state.input}
             onChange={this.handleChange}
           />
@@ -31,3 +51,7 @@ export default class Editor extends Component {
     );
   }
 }
+
+Editor.propTypes = {
+  updateMarkdown: PropTypes.func.isRequired,
+};
